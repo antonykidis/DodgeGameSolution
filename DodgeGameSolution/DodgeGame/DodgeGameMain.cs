df330abb989f17660b09ee85b5e2bc363998f006
diego98512 <31946573+diego98512@users.noreply.github.com>
2017-10-06 23:56:39 -0400

    10/6/2017
    22:56:26

---
+++ DodgeGameSolution/DodgeGame/DodgeGameMain.cs
@@ -0,0 +1,37 @@
+﻿using System; // We need the standard .NET library for many things including console
+
+namespace DodgeGame
+{
+    class DodgeGameMain
+    {
+        static void Main()
+        {
+
+
+            //Instantiate a Unit that will represent the player.
+            //Unit constructor's signature: X pos, Y pos, Char.
+            Unit playerUnit = new Unit(10, 5, "@");
+
+            //Instantiate a Unit that will represent the enemy.
+            //Unit constructor's signature: X pos, Y pos, Char.
+            Unit enemyUnit = new Unit(20, 17, "X");
+
+            //Test dummy Unit.
+            //Unit testUnit = new Unit(1, 2, "3");
+
+            //Draw Units.
+            playerUnit.Draw();
+            enemyUnit.Draw();
+            //testUnit.Draw();
+            
+            
+            //After program ends, set "Press any key to continue..." to bottom.
+
+            Console.SetCursorPosition(0, Console.WindowHeight - 1);
+
+
+        }
+    }
+}
+
+
---
+++ DodgeGameSolution/DodgeGame/Unit.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace DodgeGame
+{
+    public class Unit
+    {
+
+        public Unit(int x, int y, string UnitGraphic)
+        {
+            this.UnitGraphic = UnitGraphic;
+            this.X = x;
+            this.Y = y;
+        }
+
+
+        public int X        //The way the rest of the program interacts with X.
+        {
+            get
+            {
+                return _x;
+            }
+
+            set
+            {
+                if (value < 0 || value >= Console.WindowWidth)
+                {
+                    throw new Exception("Invalid X coordinates received.");
+                }
+
+                _x = value;
+            }
+        }
+
+        private int _x;     //Where the value of X is actually stored.
+        
+
+
+        public int Y        //The way the rest of the program interacts with Y.
+        {
+            get
+            {
+                return _y;
+            }
+
+            set
+            {
+                if (value < 0 || value >= Console.WindowHeight)
+                {
+                    throw new Exception("Invalid Y coordinates received.");
+                }
+
+                _y = value;
+            }
+        }
+
+        private int _y;     //Where the value of Y is actually stored.
+
+
+
+        public string UnitGraphic;
+
+
+
+        //This draws the unit on the screen.
+        public void Draw()
+        {
+            //This is an instance method, so if we refer
+            //to fields like x and y, we will be using
+            //the values that belong to this instance
+            //and this instance ONLY.
+            Console.SetCursorPosition(this.X, this.Y);
+            Console.Write(this.UnitGraphic);
+
+        }
+
+    }
+}
